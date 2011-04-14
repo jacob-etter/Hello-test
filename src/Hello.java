82e5880d9b82426cc405e830aa1249e5df7508df
jacob-etter <jacob.etter@colorado.edu>
2011-04-14 17:56:49 -0600

    "Hi"

--- src/Hello.java
+++ src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello World!");
-		System.out.println("Sean is an idiot");
+		System.out.println("You be dumb");
 	}
 
 }
